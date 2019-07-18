299ba1ea460d7ab956ebd43272650bae56707ecf
RANIA NAJAH <>
2019-07-18 09:39:28 +0200

    no message

---
+++ WeatherLeBonCoin/WeatherLeBonCoin/Main/Presentation/Presenter/WeatherPresenter.swift
@@ -0,0 +1,92 @@
+//
+//  WeatherPresenter.swift
+//  WeatherLeBonCoin
+//
+//  Created by RANIA NAJAH on 18/07/2019.
+//  Copyright © 2019 RANIA NAJAH. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+protocol WeatherPresenterPresenterProtocol: class {
+  var isGeolocEnabled: Bool { get }
+  var geolocAuthorizationStatus: CLAuthorizationStatus { get }
+  func requestAuthorization()
+  func fetchWeatherFrom(_ location: CLLocation?)
+  func fetchWeatherFromCurrentLocation()
+}
+
+class WeatherPresenter: WeatherPresenterPresenterProtocol {
+  
+  //Reference to the View (weak to avoid retain cycle).
+  private(set) weak var view: WeatherViewProtocol?
+  
+  var isGeolocEnabled: Bool {
+    return LocationServiceManager.shared.isServiceAvailable
+  }
+  
+  var geolocAuthorizationStatus: CLAuthorizationStatus {
+    return LocationServiceManager.shared.authorizationStatus
+  }
+  
+  init(view: WeatherViewProtocol) {
+    self.view = view
+  }
+  deinit {
+    NotificationCenter.default.removeObserver(self)
+  }
+  
+  func requestAuthorization() {
+    NotificationCenter.default.addObserver(self, selector: #selector(locationAuthorizationStatusDidChange), name: NSNotification.Name(rawValue: LocationAuthorizationStatusChangedNotification), object: nil)
+    LocationServiceManager.shared.requestAuthorization()
+    
+    view?.didRequestLocationAuthorization()
+  }
+  
+  /// Fetch Weather from given location
+  func fetchWeatherFrom(_ location: CLLocation?) {
+    
+    guard let location = location else { return }
+    
+    WeatherManager.getWeatherFrom(location: location, succes: { (weather) in
+      self.view?.didFetchWeatherSuccess(weather)
+      
+    }) { (error) in
+      self.view?.didFetchWeatherFailure(error)
+    }
+  }
+  
+  /// If geolocation is enabled, fetch weather from current location
+  func fetchWeatherFromCurrentLocation() {
+    if requestLocationAuthorization() {
+      return
+    }
+    LocationServiceManager.shared.getCurrentLocation { [weak self] (location, error) in
+      
+      if let location = location {
+        self?.fetchWeatherFrom(location)
+      } else {
+        self?.view?.didFetchWeatherFailure(error)
+      }
+    }
+  }
+  
+  @objc func locationAuthorizationStatusDidChange() {
+    view?.locationAuthorizationStatusDidChange()
+  }
+  
+  /// Check if location authorization is enabled
+  func requestLocationAuthorization() -> Bool {
+    
+    if geolocAuthorizationStatus == .notDetermined {
+      requestAuthorization()
+      return true
+      
+    } else if geolocAuthorizationStatus == .denied {
+      view?.locationAuthorizationDenied()
+      return true
+    }
+    return false
+  }
+}
---
+++ WeatherLeBonCoin/WeatherLeBonCoin/Main/Presentation/Router/WeatherRouter.swift
@@ -0,0 +1,49 @@
+//
+//  WeatherRouter.swift
+//  WeatherLeBonCoin
+//
+//  Created by RANIA NAJAH on 18/07/2019.
+//  Copyright © 2019 RANIA NAJAH. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol WeatherRouterProtocol {
+  func presentPopupLocation()
+}
+
+class WeatherRouter: WeatherRouterProtocol {
+  
+  //Reference to the View Controller (weak to avoid retain cycle).
+  private(set) weak var viewController: UIViewController?
+  
+  init(viewController: UIViewController) {
+    self.viewController = viewController
+  }
+  
+  /// Present alert to tell the user to activate position
+  func presentPopupLocation() {
+    let alert = UIAlertController(title: "Pour utiliser la géolocalisation, pensez à l’activer dans Réglages > Général > Services de localisation" , message: nil, preferredStyle: .alert)
+    
+    alert.addAction(
+      UIAlertAction(title: "Annuler", style: .cancel, handler: { _ in
+      })
+    )
+    
+    func openSettings() {
+      if let settingsUrl = URL(string: UIApplication.openSettingsURLString),
+        UIApplication.shared.canOpenURL(settingsUrl) {
+        UIApplication.shared.open(settingsUrl, options: [:])
+      }
+    }
+    
+    alert.addAction(
+      UIAlertAction(title: "Réglages", style: .default, handler: { _ in
+        openSettings()
+      })
+    )
+    
+    viewController?.present(alert, animated: true)
+  }
+}
